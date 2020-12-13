0940862965a4746b0375cf672d25d3939ccbd69d
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2020-12-13 15:15:00 -0600

    commit tree dce6b515a1352069d814bbf163e3b0e8661a4a34

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,15 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :brand
+
+  BRANDS = [ ]
 
   def initialize(brand)
     @brand = brand
+    
+    if BRANDS.any?(brand)
+      BRANDS
+    else 
+      BRANDS << brand
+    end
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
-
 end
